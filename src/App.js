76578471f1a4aa537f5c539d186fc35eb5f36bd0
kanovii <kkh0510ks@daum.net>
2021-07-09 16:04:05 +0900

    study

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let navi = '최소진 나빠요~~^^7';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          최소진 나빠요 ~~
+          {navi}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
